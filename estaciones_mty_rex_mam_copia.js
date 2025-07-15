3cb5ae12001b2f8a74a74be7ae418717a866274b
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 11:23:50 -0600

    Add files via upload

---
+++ estaciones_mty_rex_mam_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mty_rex_mam",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "PM6R+9HX", "description": "MTY1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -100.3085156, 25.7109875, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Nte. 1 190", "description": "XREX1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -98.3266874, 26.0661423, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Circón 32", "description": "MAM1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -97.5228973, 25.8492861, 0.0 ] } }
+]
+}
---
+++ paradas_mty_rex_mam_copia.js
@@ -0,0 +1,9 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mty_rex_mam",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Gasolinera Los Ahijjados", "description": "67060 China, N.L.", "layer": "PEMEX_MTY_REX_TAM", "direccion": "67060 China, N.L." }, "geometry": { "type": "Point", "coordinates": [ -99.3782114, 25.7564984, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex", "description": "Carretera Monterrey-Reynosa Km. 196+950, 88727 Reynosa, Tamps.", "layer": "PEMEX_MTY_REX_TAM", "direccion": "Carretera Monterrey-Reynosa Km. 196+950, 88727 Reynosa, Tamps." }, "geometry": { "type": "Point", "coordinates": [ -98.4435045, 26.0265921, 0.0 ] } }
+]
+}
